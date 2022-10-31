51cf5a092a1193e1431e6277bba0ac11785dd628
mayinglun2 <mayinglun2@joyy.com>
2022-10-31 16:28:20 +0800

    create project；

---
+++ StrangeWords/StrangeWordsApp.swift
@@ -0,0 +1,17 @@
+//
+//  StrangeWordsApp.swift
+//  StrangeWords
+//
+//  Created by 马英伦 on 2022/10/31.
+//
+
+import SwiftUI
+
+@main
+struct StrangeWordsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
